85c17ef03d8d4f239ea8fccff9f431a4e130fe4a
Alexander Repeta <alexander.repeta@gmail.com>
2019-12-02 14:55:03 +0200

    Add aoc animations

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
+import SmoothScroll from 'smooth-scroll';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import './sass/main.scss';
 // import './libs/bootstrap-grid.min.css';
+
+AOS.init();
+
+new SmoothScroll('a[href*="#"]', {
+  speed: 300,
+});
